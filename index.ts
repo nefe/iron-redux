9683c498a02b11a6b0a680074b27c91f8da5bda9
jasonHzq <hzongquan@126.com>
2018-06-04 20:15:07 +0800

    deploy 0.1.4

--- index.ts
+++ index.ts
@@ -306,7 +306,3 @@ export class AsyncTuple<T> {
     return state;
   }
 }
-
-export type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-  { [P in U]: never } & { [x: string]: never })[T];
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
